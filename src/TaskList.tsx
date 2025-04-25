c3a35db4cd1e0f6c3237c2fceb6b704e25836bfc
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-26 01:09:22 +0200

    created TaskList

---
+++ src/TaskList.tsx
@@ -0,0 +1,40 @@
+type TaskListPropsType = {
+    data: DataType[]
+}
+
+export type DataType = {
+    title: string
+    tasks: {
+        taskId: number
+        title: string
+        isDone: boolean
+    },
+    students: {
+        student:string
+    },
+}
+
+export const TaskList = (props: TaskListPropsType) => {
+    return (
+        <div>
+            <h1>{props.data.title}</h1>
+            <ul>
+                {props.data.tasks.map(el => {
+                    return (
+                        <li>
+                            <span>{el.taskId}</span>
+                            <span>{el.title}</span>
+                            <span>{el.isDone}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <ul>
+                {props.data.students.map(student => {
+                    return <li>{student}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
